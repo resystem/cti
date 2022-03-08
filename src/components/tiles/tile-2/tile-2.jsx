013700cf3aed073b8c7670c0d0e822cc0bb912df
cleversonlira <cleverson_lira@outlook.com>
2022-03-08 01:47:39 -0300

    [TILE-2] Creating and positioning elements

--- src/components/tiles/tile-2/tile-2.jsx
+++ src/components/tiles/tile-2/tile-2.jsx
@@ -3,7 +3,21 @@ import './tile-2.css'
 
 const Tile2 = () => (
   <div className="tile __2">
+
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-2/illustrations/Kerexu_IMG_8902.png" />
+
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-2/illustrations/desenhos-AraPoty9.jpg" />
     
+    <img draggable="false" className="illustration __3" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-2/illustrations/desenho-Sophia12.jpg" />
+
+    <div className='video __2'>
+      <div className="thumb-wrapper">
+        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-2/film-thumbnail/thumb-filme-3-Conselhos-Mbya-Guarani_pb.png"/>
+        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+      </div>
+    </div>
+
+    <img draggable="false" className="illustration __4" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-2/illustrations/desenhos-AraPoty11.jpg" />
   </div>
 );
 
