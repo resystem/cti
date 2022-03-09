d3e291adaa8c604aa093c34e6c7260e223c31b40
cleversonlira <cleverson_lira@outlook.com>
2022-03-08 22:18:00 -0300

    [TILE-6] Creating and positioning elements

--- src/components/tiles/tile-6/tile-6.jsx
+++ src/components/tiles/tile-6/tile-6.jsx
@@ -3,7 +3,13 @@ import './tile-6.css'
 
 const Tile6 = () => (
   <div className="tile __6">
-    
+
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-6/illustrations/Paty_IMG_20210429_184206.png" />
+
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-6/illustrations/Julia_1_mundo.png" />
+
+    <img draggable="false" className="illustration __3" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-6/illustrations/Digitalizado_20210429-1847.png" />
+
   </div>
 );
 
