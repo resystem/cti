a6b8b07d829afbc079e0cba8207706c855ed76e9
cleversonlira <cleverson_lira@outlook.com>
2022-03-12 12:13:34 -0300

    [TILE-10] Creating and positioning elements

--- src/components/tiles/tile-10/tile-10.jsx
+++ src/components/tiles/tile-10/tile-10.jsx
@@ -3,7 +3,20 @@ import './tile-10.css'
 
 const Tile10 = () => (
   <div className="tile __10">
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-10/illustrations/Kerexu_IMG_8899.png" />
+
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-10/illustrations/borboleta.jpg" />
+
+    <img draggable="false" className="illustration __3" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-10/illustrations/not-found.JPG" />
     
+    <div className='video __1'>
+      <div className="thumb-wrapper">
+        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-10/film-thumbnail/filme4_AYVU_RAPYTA_pb.png" />
+        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+      </div>
+    </div>
+
+    <img draggable="false" className="illustration __4" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-10/illustrations/fogoecidade_pg.jpg" />
   </div>
 );
 
