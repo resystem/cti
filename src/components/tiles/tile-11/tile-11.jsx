b377d9fb156e7f2ecb7c51d8bdae3909dbe23bc0
cleversonlira <cleverson_lira@outlook.com>
2022-03-12 12:29:55 -0300

    [TILE-11] Creating and positioning elements

--- src/components/tiles/tile-11/tile-11.jsx
+++ src/components/tiles/tile-11/tile-11.jsx
@@ -3,7 +3,17 @@ import './tile-11.css'
 
 const Tile11 = () => (
   <div className="tile __11">
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/illustrations/pesemente.jpg" />
     
+    <div className='video __1'>
+      <div className="thumb-wrapper">
+        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/film-thumbnail/depoimento6_frame_pb.png" />
+        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
+      </div>
+    </div>
+
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/illustrations/Paty_Digitalizado_20210621-1235.png" />
+
   </div>
 );
 
