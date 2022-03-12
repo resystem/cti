58811c95214a15196d738e69b21c461536c0bece
cleversonlira <cleverson_lira@outlook.com>
2022-03-12 10:16:31 -0300

    [TILE-7] Creating and positioning elements

--- src/components/tiles/tile-7/tile-7.jsx
+++ src/components/tiles/tile-7/tile-7.jsx
@@ -3,6 +3,16 @@ import './tile-7.css'
 
 const Tile7 = () => (
   <div className="tile __7">
+
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-7/illustrations/forma_1.png" />
+
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-7/illustrations/Kerexu_IMG_8905.png" />
+
+    <img draggable="false" className="illustration __3" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-7/illustrations/Paty_Digitalizado_20210429-1844.png" />
+
+    <img draggable="false" className="illustration __4" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-7/illustrations/Julia_5_aldeia2.png" />
+
+    <img draggable="false" className="illustration __5" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-7/illustrations/forma_5.png" />
     
   </div>
 );
