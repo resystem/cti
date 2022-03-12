ef699099b61d3716213bb7a467b745c10c7c4fb2
cleversonlira <cleverson_lira@outlook.com>
2022-03-12 12:44:41 -0300

    [TILE-12] Creating and positioning elements

--- src/components/tiles/tile-12/tile-12.jsx
+++ src/components/tiles/tile-12/tile-12.jsx
@@ -3,7 +3,11 @@ import './tile-12.css'
 
 const Tile12 = () => (
   <div className="tile __12">
+    <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-12/illustrations/Paty_IMG_20210429_184223.png" />
+
+    <img draggable="false" className="photo __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-12/photos/GIF_6_OPY_C.png" />
     
+    <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-12/illustrations/not-found.png" />
   </div>
 );
 
