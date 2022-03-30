a9e5e6d6bbc1d64a651c2a3110de7eb3505ac6a1
cleversonlira <cleverson_lira@outlook.com>
2022-03-29 22:46:16 -0300

    [TILE-11] Refactoring movie div to component VideoWrapper

--- src/components/tiles/tile-11/tile-11.jsx
+++ src/components/tiles/tile-11/tile-11.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { actions } from '../../../store';
+import VideoWrapper from '../../video/video';
 import './tile-11.css'
 
-const Tile11 = () => (
+const video1 = {
+  pt: 'https://cti-assets.s3.us-west-2.amazonaws.com/pexels-ekaterina-bolovtsova-7003250.mp4',
+};
+
+const Tile11 = ({ state, dispatch }) => (
   <div className="tile __11">
     <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/illustrations/pesemente.jpg" />
-    
-    <div className='video __1'>
-      <div className="thumb-wrapper">
-        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/film-thumbnail/depoimento6_frame_pb.png" />
-        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
-      </div>
-    </div>
+
+    <VideoWrapper
+      id="__1"
+      video={state.video}
+      thumbnail="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/film-thumbnail/depoimento6_frame_pb.png"
+      src={video1[state.language]}
+      setVideo={(data) => {
+        dispatch({
+          type: actions.SET_VIDEO,
+          data,
+        })
+      }}
+    />
 
     <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-11/illustrations/Paty_Digitalizado_20210621-1235.png" />
 
