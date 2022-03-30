889011455b081858aa3484a92633a8974f61ec63
cleversonlira <cleverson_lira@outlook.com>
2022-03-29 22:43:46 -0300

    [TILE-5] Refactoring movie div to component VideoWrapper

--- src/components/tiles/tile-5/tile-5.jsx
+++ src/components/tiles/tile-5/tile-5.jsx
@@ -1,26 +1,49 @@
 import React from 'react'
+import { actions } from '../../../store';
+import VideoWrapper from '../../video/video';
 import './tile-5.css'
 
-const Tile5 = () => (
-  <div className="tile __5">
+const video1 = {
+  pt: 'https://cti-assets.s3.us-west-2.amazonaws.com/pexels-ekaterina-bolovtsova-7003250.mp4',
+  en: 'https://cti-assets.s3.us-west-2.amazonaws.com/pexels-ekaterina-bolovtsova-7003250.mp4',
+  es: 'https://cti-assets.s3.us-west-2.amazonaws.com/pexels-ekaterina-bolovtsova-7003250.mp4',
+};
 
-    <div className='video __1'>
-      <div className="thumb-wrapper">
-        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/film-thumbnail/filme2_AVA_MOCOI_pb.png" />
-        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
-      </div>
-    </div>
+const video2 = {
+  pt: 'https://cti-assets.s3.us-west-2.amazonaws.com/pexels-ekaterina-bolovtsova-7003250.mp4',
+};
+
+const Tile5 = ({ state, dispatch }) => (
+  <div className="tile __5">
+    <VideoWrapper
+      id="__1"
+      video={state.video}
+      thumbnail="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/film-thumbnail/filme2_AVA_MOCOI_pb.png"
+      src={video1[state.language]}
+      setVideo={(data) => {
+        dispatch({
+          type: actions.SET_VIDEO,
+          data,
+        })
+      }}
+    />
 
     <img draggable="false" className="illustration __1" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/illustrations/desenhos-AraPoty4.jpg" />
 
     <img draggable="false" className="illustration __2" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/illustrations/desenhos-AraPoty6.jpg" />
 
-    <div className='video __2'>
-      <div className="thumb-wrapper">
-        <img draggable="false" className="thumb" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/film-thumbnail/depoimento2_frame_pb.png" />
-        <svg className="play" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ffffff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
-      </div>
-    </div>
+    <VideoWrapper
+      id="__2"
+      video={state.video}
+      thumbnail="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/film-thumbnail/depoimento2_frame_pb.png"
+      src={video2[state.language]}
+      setVideo={(data) => {
+        dispatch({
+          type: actions.SET_VIDEO,
+          data,
+        })
+      }}
+    />
 
     <img draggable="false" className="illustration __3" src="https://cti-assets.s3.us-west-2.amazonaws.com/tile-5/illustrations/janela.jpg" />
 
