bc1a095b093a16f9c808204bd3a75ea4d0837f6c
Gabriel Furlan <gabrielfurlan05@gmail.com>
2022-04-08 08:48:32 -0300

    adding provider on ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+import React from 'react';
+import { StoreComponent } from './src/store';
+
+export const wrapRootElement = ({ element }) => (
+  <StoreComponent>{element}</StoreComponent>
+);
